09bfa5195d2737ff33bfb806eefe217f56b903d6
CyzmiX <98268356+CyzmiX@users.noreply.github.com>
2023-11-29 18:45:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ import injectSocketIo from "./socketinject.js"; // The SocketIO stuff (see next 
 import express from 'express';
 const app = express();
 const server = http.createServer(app);
-
+const port = process.env.PORT ?? 8000
 // Inject SocketIO
 injectSocketIo(server)
 
 // SvelteKit handlers
 app.use(handler);
 
-server.listen(process.env.PORT || 3000, () => {
+server.listen(port, () => {
   console.log('Running on http://localhost:3000');
 });
